f10ca1622933f0a9efd0fc56d9fc66538004a6e8
Hrehorowicz <sewer.hrehorowicz@productmadness.com>
2024-04-12 12:05:39 +0200

    refactoring storage.js to explicitly refer to pluginData

--- storage.js
+++ storage.js
@@ -1,27 +1,48 @@
 const fs = require('uxp').storage.localFileSystem;
 
 const storage = {
-  datafile: "pluginData.json",
+  pluginDataFile: "pluginData.json",
 
-  data: {},
+  pluginData: {},
+
+  getOrCreateFolder: async function(path) {
+    try {
+      await fs.getEntryWithUrl(path);
+    } catch (e) {
+      await fs.createEntryWithUrl(path, { type: "folder" })
+    }
+  },
 
   pathToUrl: function (path) {
     let newPath = path.replace(/\\/g, "/");
     return newPath.replace(/^([A-Z]):/i, (match, driveLetter) => `file:/${driveLetter.toLowerCase()}:`);
   },
 
-  load: function () {
-
+  loadPluginData: async function () {
+    try {
+      const jsonFile = await fs.createEntryWithUrl(`plugin-data:/${this.pluginDataFile}`, {overwrite: true});
+      console.log("is file? " + jsonFile.isFile);
+      const fileContent = await jsonFile.read();
+      this.pluginData = JSON.parse(fileContent);
+    } catch (e) {
+      if (e.code == -4058) {
+        console.log(`${this.pluginDataFile} hasn't beed created yet, ignoring.`);
+      } else {
+        console.error("loading data error: " + e);
+      }
+      return;
+    }
+    console.log(`${this.pluginDataFile} imported into storage.data`);
   },
 
-  save: async function (data) {
+  savePluginData: async function (data) {
     try {
-      const _data = data || this.data;
-      const file = await fs.createEntryWithUrl(`plugin-data:/${this.datafile}`, { overwrite: true });
+      const _data = data || this.pluginData;
+      const file = await fs.createEntryWithUrl(`plugin-data:/${this.pluginDataFile}`, { overwrite: true });
       await file.write(JSON.stringify(_data));
       console.log(`Data saved to: ${file.nativePath}`);
     } catch (e) {
-      console.error(e);
+      console.error("saving data error: " + e);
     }
   }
 }
