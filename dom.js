73d9a776a88a88e2a704af5e131844d2559b59dc
Sewer <sewer.cvlt@gmail.com>
2024-05-21 09:45:09 +0200

    handling dynamically created markup (templates)

--- dom.js
+++ dom.js
@@ -1,39 +1,55 @@
 /**
- * Methods for handling templates
+ * Methods for handling templates;
+ * this becomes handler
  */
 const templatesExtension = {
   templates: {},
 
-  cloneTemplate: function(name, variables, container) {
+  /**
+   * Clones a template element, handles variables, actions, and stores references.
+   *
+   * @param {Object} [params={}] - Parameters for cloning and handling a template.
+   * @param {string} params.name - The name of the template to clone.
+   * @param {Object} [params.variables] - Optional variables to replace in the cloned template.
+   * @param {Element} [params.container] - Optional container element to append the cloned template.
+   *
+   * @returns {Element|undefined} Returns the cloned template or undefined if container is specified.
+   *
+   * @description
+   * Clones the template specified by `name`, handles variables and actions defined within the template, and optionally appends the cloned template to a container element.
+   * If `variables` are provided, replaces corresponding data-variable attributes in the cloned template with the values.
+   * Stores variables and references within the cloned template for future access in event handlers.
+   * Recursively attaches event listeners to elements and their descendants based on data-action attributes.
+   * Stores references within the cloned template using `dom.handleRefs` for access in event handlers.
+   */
+  cloneTemplate: function ({name, variables, container} = {}) {
     let template = this.templates[name];
     if (typeof template !== "object") {
       console.error(`Trying to clone "${name}" template, but it doesn't exist in ${this.name}`);
     }
-    let clone = template.cloneNode(true);
-    clone.classList.add(clone.dataset.template);
-    clone.removeAttribute("data-template");
-    
+    let templateClone = template.cloneNode(true);
+    templateClone.classList.add(templateClone.dataset.template);
+    templateClone.removeAttribute("data-template");
+
     // handle variables if any passed
     if (typeof variables === "object") {
       for (let key in variables) {
         let value = variables[key];
-        let node = clone.querySelector(`[data-variable="${key}"]`);
+        let node = templateClone.querySelector(`[data-variable="${key}"]`);
         if (typeof node === "object")
           node.innerHTML = value;
       }
     }
+    templateClone.variables = variables; // store variables to access them later
 
-    let actions = clone.querySelectorAll(`[data-action]`);
-    actions.forEach(action => {
-      let actionName = action.dataset.action;
-      // common function to handle individual action in dom, to be implemented
-      console.log(`trying to handle ${actionName} for ${this.name}`);
-    })
+    dom.handleRefs(templateClone, templateClone); // also store refs for templace, so they can be accessed in event handlers
+
+    dom.handleActionsRecursively({elem: templateClone, handler: this, template: templateClone});
 
     if (typeof container === "object") {
-      container.appendChild(clone);
+      container.appendChild(templateClone);
     } else {
-      return clone;
+      return templateClone;
     }
   }
 };
@@ -44,12 +60,13 @@ const templatesExtension = {
 const dom = {
   handlersRegistry: {},
 
-  extend: function(handler, extension) {
+  extend: function (handler, extension) {
     for (let each in extension) {
       handler[each] = extension[each];
     }
   },
 
+  // new main function to register handler once & handle all stuff
   registerHandler: function (handler, name = handler.constructor.name) {
     handler.name = name;
     this.handlersRegistry[name] = handler;
@@ -65,6 +82,77 @@ const dom = {
     return true;
   },
 
+  
+
+  /**
+   * Recursively attaches event listeners to elements and their descendants based on data-action attributes.
+   *
+   * @param {Object} [params={}] - Parameters for handling actions recursively.
+   * @param {Element} params.elem - The root DOM element from which to start handling actions and iterate through descendants.
+   * @param {Object} params.handler - The handler object containing action methods.
+   * @param {*} params.template - Template reference passed down to event handlers.
+   *
+   * @description
+   * This function iterates over all elements and their descendants starting from `elem`. It looks for `data-action-*` attributes on each element and attaches event listeners based on these attributes.
+   * Each `data-action-*` attribute specifies an action to be handled by `handler` when the corresponding event (`click` by default) occurs on the element.
+   */
+  handleActionsRecursively: function ({elem, handler, template} = {}) {
+    for (let key in elem.dataset) {
+      if (key.startsWith("action")) {
+        const splittedType = key.split("action")[1];
+        const type = splittedType == "" ? "click" : splittedType.toLowerCase();
+        const action = elem.dataset[key];
+        elem.template = template;
+        this.handleActionForElem({ 
+          elem: elem, 
+          handler: handler, 
+          action: action, 
+          type: type,
+          template: template
+        });
+      }
+    }
+
+    Array.from(elem.children).forEach(child => this.handleActionsRecursively({
+      elem: child, 
+      handler: handler, 
+      template: template
+    }));
+  },
+
+  /**
+  * Attaches an event listener to a specified element for a given action method from a handler object.
+  *
+  * @param {Object} args - The arguments for handling the action.
+  * @param {Object} args.handler - The handler object containing the action method.
+  * @param {string} args.action - The name of the action method in the handler object to be invoked.
+  * @param {Element} args.elem - The DOM element to which the event listener will be attached.
+  * @param {string} args.type - The type of event (e.g., "click", "mouseover") to listen for on the element.
+  *
+  * @throws Will throw an error if the handler is not passed, is not an object, or does not contain the specified action method.
+  */
+  handleActionForElem: function ({ handler, action, elem, type, template } = {}) {
+    try {
+      if (typeof handler === "undefined") throw "no handler passed";
+      if (typeof handler !== "object") throw "handler is not an object";
+      if (typeof handler[action] !== "function") throw `Missing ${handler.name}.${action}() action.`;
+      elem.template = template;
+      elem.addEventListener(type, handler[action].bind(handler));
+    } catch (e) {
+      console.error("error handling action: " + e);
+    }
+  },
+
+  /**
+   * Main function to handle actions, call in once on start.
+   * By default grabs actions from whole document. Override actionsContainer if you don't want this behavior.
+   * @param {*} handler 
+   * @param {*} actionsContainer 
+   */
+  handleActions: function (handler, actionsContainer = document) {
+    this.handleActionsRecursively({elem: actionsContainer, handler: handler});
+  },
+
   /**
    * Handles reference for single dom element.
    * References are set in [data-ref="refName"], 
@@ -74,53 +162,25 @@ const dom = {
    * @returns 
    */
   handleRef: function (elem, handler, refName) {
+    console.log("handling refs for elem " + elem.innerText);
+    console.dir(elem);
     if (!handler.refs)
       handler.refs = {};
     refName ||= elem.dataset.ref;
     handler.refs[refName] = elem;
+    console.dir(handler);
   },
 
-  handleActionType: function (handler, type) {
-    try {
-      if (!this.hasHandler(handler))
-        return;
-      const selector = type == "click" ? "[data-action]" : `[data-action-${type}]`;
-      const actionType = type == "click" ? "action" : "action" + type[0].toUpperCase() + type.slice(1);
-      const elems = document.querySelectorAll(selector);
-      elems.forEach(elem => {
-        const actionName = elem.dataset[actionType];
-        if (typeof handler[actionName] === "undefined") {
-          console.error(`No matching function found for ${actionName} in ${handler.constructor.name}`);
-        } else {
-          if (type == "init") { // init action passes element, not event
-            handler[actionName](elem);
-          } else {
-            elem.addEventListener(type, handler[actionName].bind(handler));
-          }
-
-          this.handleRef(elem, handler, actionName);
-        }
-      });
-    } catch (e) {
-      console.error("actions handling error " + e);
-    }
-  },
-
-  handleActions: function (handler) {
-    this.handleActionType(handler, "init"); // @todo we probably don't need it
-    this.handleActionType(handler, "click");
-    this.handleActionType(handler, "input");
-  },
-
-  handleRefs: function (handler) {
+  handleRefs: function (handler, container = document) {
     if (!this.hasHandler(handler))
       return;
-    const elems = document.querySelectorAll("[data-ref]")
+    const elems = container.querySelectorAll("[data-ref]")
     elems.forEach(elem => {
       this.handleRef(elem, handler);
     });
   },
 
+  // internal function
   handleTemplates: function (handlerName) {
     console.log("trying handle templates");
     const templatesRoots = document.querySelectorAll(`[data-templates-for="${handlerName}"]`);
@@ -134,7 +194,7 @@ const dom = {
           if (typeof handler.templates !== "object")
             handler.templates = {};
           handler.templates[template.dataset.template] = template;
-          
+
         });
       } else {
         console.error(`You're trying to handle templates for ${handlerName}, but there's no handler assiciated with it. Perhaps you forgot for call dom.registerHandler(${handlerName})?`)
